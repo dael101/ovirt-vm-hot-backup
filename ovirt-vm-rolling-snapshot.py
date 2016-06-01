db69ee64939603c2dc73076c2ca2af8f0cbce3ce
dael101 <dael.maselli@gmail.com>
2016-06-01 09:53:08 +0200

    refactoring

--- ovirt-vm-rolling-snapshot.py
+++ ovirt-vm-rolling-snapshot.py
@@ -9,18 +9,18 @@ import os
 from operator import attrgetter
 
 scriptdir = os.path.abspath(os.path.dirname(sys.argv[0]))
-conffile  = scriptdir + "/ovirt-vm-rolling-snapshot.conf"
+conffile = scriptdir + "/ovirt-vm-rolling-snapshot.conf"
 
 Config = ConfigParser.ConfigParser()
 
-if not os.path.isfile( conffile ):
-    print "Config file %s does not exists. Exiting." % ( conffile )
+if not os.path.isfile(conffile):
+    print "Config file %s does not exists. Exiting." % conffile
     sys.exit(1)
 
-Config.read( conffile )
+Config.read(conffile)
 
-if len( Config.sections() ) < 1:
-    print "Config file is not valid. Exiting." 
+if len(Config.sections()) < 1:
+    print "Config file is not valid. Exiting."
     sys.exit(1)
 
 basetime = time
@@ -31,16 +31,16 @@ for vmname in Config.sections():
 
     try:
 
-        etime_to_keep   = int(Config.get(vmname, 'etime_to_keep'))
-        hourly_to_keep  = int(Config.get(vmname, 'hourly_to_keep'))
-        daily_to_keep   = int(Config.get(vmname, 'daily_to_keep'))
-        weekly_to_keep  = int(Config.get(vmname, 'weekly_to_keep'))
+        etime_to_keep = int(Config.get(vmname, 'etime_to_keep'))
+        hourly_to_keep = int(Config.get(vmname, 'hourly_to_keep'))
+        daily_to_keep = int(Config.get(vmname, 'daily_to_keep'))
+        weekly_to_keep = int(Config.get(vmname, 'weekly_to_keep'))
         monthly_to_keep = int(Config.get(vmname, 'monthly_to_keep'))
 
-        time_hours      = "%02d" % int(Config.get(vmname, 'time_hours'))
-        time_minutes    = "%02d" % int(Config.get(vmname, 'time_minutes'))
-        time_weekday    = "%d"   % int(Config.get(vmname, 'time_weekday'))
-        time_monthweek  = int(Config.get(vmname, 'time_monthweek'))
+        time_hours = "%02d" % int(Config.get(vmname, 'time_hours'))
+        time_minutes = "%02d" % int(Config.get(vmname, 'time_minutes'))
+        time_weekday = "%d" % int(Config.get(vmname, 'time_weekday'))
+        time_monthweek = int(Config.get(vmname, 'time_monthweek'))
 
         if time_monthweek < 1 or time_monthweek > 5:
             time_monthweek = 1
@@ -48,27 +48,30 @@ for vmname in Config.sections():
         if time_weekday == "7":
             time_weekday = "0"
 
-        last_to_keep = { "____":etime_to_keep, "H___":hourly_to_keep, "HD__":daily_to_keep, "HDW_":weekly_to_keep, "HDWM":monthly_to_keep }
+        last_to_keep = {"____": etime_to_keep, "H___": hourly_to_keep, "HD__": daily_to_keep, "HDW_": weekly_to_keep,
+                        "HDWM": monthly_to_keep}
 
         hpos = dpos = wpos = mpos = "_"
 
-        if basetime.strftime("%M") == time_minutes: # minutes is 00
+        if basetime.strftime("%M") == time_minutes:  # minutes is 00
             hpos = "H"
 
-            if basetime.strftime("%H") == time_hours: # hour is 00
+            if basetime.strftime("%H") == time_hours:  # hour is 00
                 dpos = "D"
 
-                if basetime.strftime("%w") == time_weekday: # day of week is sunday
+                if basetime.strftime("%w") == time_weekday:  # day of week is sunday
                     wpos = "W"
 
-                    if int( basetime.strftime("%d") ) <= ( 7 * time_monthweek ) and \
-                       int( basetime.strftime("%d") ) > ( 7 * ( time_monthweek - 1 ) ): # is the first week of month
+                    if (int(basetime.strftime("%d")) <= (7 * time_monthweek)) and (
+                        int(basetime.strftime("%d")) > (7 * (time_monthweek - 1))):  # is the first week of month
                         mpos = "M"
-
 
         snap_time_id = hpos + dpos + wpos + mpos
 
         if last_to_keep[snap_time_id]:
+
+            print ""
+            print "VM name: " + vmname
 
             try:
                 ovirtsdk
@@ -77,24 +80,22 @@ for vmname in Config.sections():
                 from ovirtsdk.xml import params
 
             api = ovirtsdk.api.API(
-                    url=Config.get(vmname, 'server'),
-                    username=Config.get(vmname, 'username'),
-                    password=Config.get(vmname, 'password'),
-                    insecure=True,
-                    debug=False
+                url=Config.get(vmname, 'server'),
+                username=Config.get(vmname, 'username'),
+                password=Config.get(vmname, 'password'),
+                insecure=True,
+                debug=False
             )
 
             vm = api.vms.get(vmname)
 
-            print ""
-            print "VM name: " + vm.get_name()
-            print "Begin backup of VM '%s' at %s" % ( vmname, datetime.datetime.now().isoformat(" ") )
-            print "VM status: %s" % str( vm.get_status().state )
+            print "Begin backup of VM '%s' at %s" % (vmname, datetime.datetime.now().isoformat(" "))
+            print "VM status: %s" % str(vm.get_status().state)
 
             snap_description = "Rolling snapshot " + snap_time_id + " at " + datetime.datetime.now().isoformat(" ")
             print "Creating Snapshot " + snap_description
 
-            snapcreation = vm.snapshots.add( params.Snapshot(description=snap_description) )
+            snapcreation = vm.snapshots.add(params.Snapshot(description=snap_description))
 
             snaptoclone = ""
             snap_status = ""
@@ -104,7 +105,7 @@ for vmname in Config.sections():
                 snap_status = snaptoclone.get_snapshot_status()
                 if snap_status != "locked":
                     break
-                #print "Snapshot in progress (" + snap_status + ") ..."
+                    # print "Snapshot in progress (" + snap_status + ") ..."
 
             if snap_status != "ok":
                 print "Snapshot creation failed. Status: " + snap_status
@@ -113,7 +114,7 @@ for vmname in Config.sections():
             print "Snapshot done"
             time.sleep(1)
 
-            snapshots_param = params.Snapshots( snapshot=[params.Snapshot( id= snaptoclone.get_id())] )
+            snapshots_param = params.Snapshots(snapshot=[params.Snapshot(id=snaptoclone.get_id())])
 
             print "Launch delete snapshot..."
             snaptodel = []
@@ -122,8 +123,8 @@ for vmname in Config.sections():
                 snapi_descr = vm.snapshots.get(id=snapi_id).description
                 snapi_time_match = re.match('^Rolling snapshot ' + snap_time_id + ' at', snapi_descr)
                 if snapi_time_match:
-                    snaptodel.append( snapi )
-            snaptodel = sorted(snaptodel, key=attrgetter('creation_time') )
+                    snaptodel.append(snapi)
+            snaptodel = sorted(snaptodel, key=attrgetter('creation_time'))
             for snapitodel in snaptodel:
                 print "Snapshot: " + snapitodel.description
 
@@ -137,15 +138,14 @@ for vmname in Config.sections():
                 snapitodel.delete(async=False)
                 while vm.snapshots.get(id=snapitodel.get_id()):
                     time.sleep(5)
-                    #print "Snapshot in progress (" + snap_status + ") ..."
+                    # print "Snapshot in progress (" + snap_status + ") ..."
                 print "Delete snapshot done"
 
             eltime = time.time() - starttime
-            print "Finished backup of VM '%s' at %s. %d seconds." % ( vmname, datetime.datetime.now().isoformat(" "), eltime )
-
+            print "Finished backup of VM '%s' at %s. %d seconds." % (vmname,
+                                                                     datetime.datetime.now().isoformat(" "),
+                                                                     eltime)
 
     except Exception, e:
         print e
         print "Backup ERROR!!!"
-
-
